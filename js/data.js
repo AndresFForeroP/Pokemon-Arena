659b7dd7a585ffe493d13f9c12014341af6a6fd3
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-05-12 16:49:41 -0500

    feat: :sparkles: Agregado boostrap y swetalert2

---
+++ js/data.js
@@ -0,0 +1,119 @@
+export const Pokemones = [{
+    id:"722",
+    nombre:"Rowlet",
+    tipo:"Planta",
+    generacion:7,
+    vida:"32",
+    defensa:"5",
+    ataque:"5",
+    habilidades:{
+        "Placaje":1,
+        "Picotazo":1.2,
+        "Hoja afilada":1.5,
+        "Pajaro osado":1.4
+    },
+    imagen:"./storage/img/rowlet.jpg"
+},{
+    id: "143",
+    nombre: "Snorlax",
+    tipo: "Normal",
+    generacion: 1,
+    vida: "50",
+    defensa: "10",
+    ataque: "4",
+    habilidades: {
+        "Placaje": 1,
+        "Lenguetazo": 1.2,
+        "Golpe Cuerpo": 1.4,
+        "Terremoto": 1.6
+    },
+    imagen: "./storage/img/snorlax.jpg"
+},{
+    id: "727",
+    nombre: "Incineroar",
+    tipo: "Fuego",
+    generacion: 7,
+    vida: "30",
+    defensa: "9",
+    ataque: "8",
+    habilidades: {
+        "Placaje": 1,
+        "Lanzallamas": 1.5,
+        "Garra umbría": 1.4,
+        "Patada baja": 1.3
+    },
+    imagen: "./storage/img/incineroar.jpg"
+},
+{
+    id: "6",
+    nombre: "Charizard",
+    tipo: "Fuego",
+    generacion: 1,
+    vida: "39",
+    defensa: "7",
+    ataque: "9",
+    habilidades: {
+        "Placaje": 1,
+        "Lanzallamas": 1.5,
+        "Vuelo": 1.3,
+        "Giro fuego": 1.4
+    },
+    imagen: "./storage/img/charizard.jpg"
+},{
+    id: "802",
+    nombre: "Zeraora",
+    tipo: "Eléctrico",
+    generacion: 7,
+    vida: "40",
+    defensa: "7",
+    ataque: "9",
+    habilidades: {
+        "Placaje": 1,
+        "Puño voltio": 1.5,
+        "Puño fuego": 1.4,
+        "Fuerza telúrica": 1.6
+    },
+    imagen: "./storage/img/zeraora.jpg"
+},{
+    id: "25",
+    nombre: "Pikachu",
+    tipo: "Eléctrico",
+    generacion: 1,
+    vida: "35",
+    defensa: "4",
+    ataque: "6",
+    habilidades: {
+        "Placaje": 1,
+        "Impactrueno": 1.5,
+        "Rayo": 1.6,
+        "Electrobola": 1.4
+    },
+    imagen: "./storage/img/pikachu.jpg"
+},{
+    id: "129",
+    nombre: "Magikarp",
+    tipo: "Agua",
+    generacion: 1,
+    vida: "20",
+    defensa: "3",
+    ataque: "2",
+    habilidades: {
+        "Salpicadura": 1
+    },
+    imagen: "./storage/img/magikarp.jpg"
+},{
+    id: "730",
+    nombre: "Primarina",
+    tipo: "Agua/Hada",
+    generacion: 7,
+    vida: "50",
+    defensa: "8",
+    ataque: "8",
+    habilidades: {
+        "Placaje": 1,
+        "Burbujas": 1.5,
+        "Canto mortal": 1.6,
+        "Cascada": 1.4
+    },
+    imagen: "./storage/img/primarina.jpg"
+}]
