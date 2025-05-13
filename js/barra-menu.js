6d981831e0c9ec564e3c48e6a6e3072323f6bec4
AndresFForeroP <andres2200500@correo.uis.edu.co>
2025-05-12 19:37:52 -0500

    feat: :sparkles: Pagina principal terminada

---
+++ js/barra-menu.js
@@ -0,0 +1,349 @@
+const home = `.barra-menu{
+    a{
+        text-decoration: none;
+    }
+    display: grid;
+    grid-template-columns: 15% 21% 21% 21% 21%;
+    background-color: var(--rojo);
+    width: 101%;
+    height: 6rem;
+    margin-top: -0.5rem;
+    margin-left: -0.5rem;
+    img{
+        margin-top: 0.3rem;
+        width: 4.5rem;
+        margin-left: 2.3rem;
+    }
+    div{
+        margin: 0.5rem;
+        margin-left: 1rem;
+    }
+    .primera-menu{
+        margin-left: -1rem;
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .primera-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .home-menu{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .septima-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .septima-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .batalla-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .batalla-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+}`
+const primera = `.barra-menu{
+    a{
+        text-decoration: none;
+    }
+    display: grid;
+    grid-template-columns: 15% 21% 21% 21% 21%;
+    background-color: var(--rojo);
+    width: 101%;
+    height: 6rem;
+    margin-top: -0.5rem;
+    margin-left: -0.5rem;
+    img{
+        margin-top: 0.3rem;
+        width: 4.5rem;
+        margin-left: 2.3rem;
+    }
+    div{
+        margin: 0.5rem;
+        margin-left: 1rem;
+    }
+    .primera-menu{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .home-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .home-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .septima-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .septima-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .batalla-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .batalla-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+}`
+const septima = `.barra-menu{
+    a{
+        text-decoration: none;
+    }
+    display: grid;
+    grid-template-columns: 15% 21% 21% 21% 21%;
+    background-color: var(--rojo);
+    width: 101%;
+    height: 6rem;
+    margin-top: -0.5rem;
+    margin-left: -0.5rem;
+    img{
+        margin-top: 0.3rem;
+        width: 4.5rem;
+        margin-left: 2.3rem;
+    }
+    div{
+        margin: 0.5rem;
+        margin-left: 1rem;
+    }
+    .primera-menu{
+        margin-left: -1rem;
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .primera-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .home-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .septima-menu{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .home-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .batalla-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .batalla-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+}`
+const batalla = `.barra-menu{
+    a{
+        text-decoration: none;
+    }
+    display: grid;
+    grid-template-columns: 15% 21% 21% 21% 21%;
+    background-color: var(--rojo);
+    width: 101%;
+    height: 6rem;
+    margin-top: -0.5rem;
+    margin-left: -0.5rem;
+    img{
+        margin-top: 0.3rem;
+        width: 4.5rem;
+        margin-left: 2.3rem;
+    }
+    div{
+        margin: 0.5rem;
+        margin-left: 1rem;
+    }
+    .primera-menu{
+        margin-left: -1rem;
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .primera-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .home-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .septima-menu{
+        margin-top: 1.7rem;
+        color: var(--gris);
+    }
+    .septima-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+    .batalla-menu{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);;
+    }
+    .home-menu:hover{
+        margin-top: 1.5rem;
+        font-size: 2.4rem;
+        color: var(--amarrillo);
+        text-shadow:
+        -3px -3px 0 var(--azul),
+        3px -3px 0 var(--azul),
+        2px  7px 0 rgb(0, 0, 0,0.4),
+        3px  3px 0 var(--azul);
+    }
+}`
+export class barraBusqueda extends HTMLElement{
+    constructor() {
+        super();
+        this.attachShadow({mode:'open'})
+        this._categoria = "";
+        this.pagina = document.createElement('div')
+        this.pagina.classList.add('barra-menu');
+        this.pagina.innerHTML = `
+        <div class="icono-menu">
+            <img src="./storage/img/logo-barra.png" alt="">
+        </div>
+        <div >
+            <a href=""><h1 class="home-menu">HOME</h1></a>
+        </div>
+        <div >
+            <a href=""><h1 class="primera-menu">1ra GEN</h1></a>
+        </div>
+        <div>
+            <a href=""><h1 class="septima-menu">7ra GEN</h1></a>
+        </div>
+        <div>
+            <a href=""><h1 class="batalla-menu">BATTLE</h1></a>
+        </div>
+        `
+        this.shadowRoot.appendChild(this.pagina);
+    }
+    set categoria(value){
+        this._categoria = value;
+        this.render();
+    }
+    render(){
+        this.estilos = document.createElement('style')
+        this.shadowRoot.innerHTML = "";
+        this.shadowRoot.appendChild(this.pagina.cloneNode(true));
+        switch (this._categoria) {
+            case "menu":
+                this.estilos.innerHTML = `${home}`
+                this.shadowRoot.appendChild(this.estilos);
+                break;
+            case "primera":
+                this.estilos.innerHTML = `${primera}`
+                this.shadowRoot.appendChild(this.estilos);
+                break;
+            case "septima":
+                this.estilos.innerHTML = `${septima}`
+                this.shadowRoot.appendChild(this.estilos);
+                break;
+            case "batalla":
+                this.estilos.innerHTML = `${batalla}`
+                this.shadowRoot.appendChild(this.estilos);
+                break;
+        }
+    }
+}
